b335ae7bef1688735a6c2fd504bdfc3eee99bde8
levanloc <levanloc7520@gmail.com>
2023-12-21 00:29:26 +0700

    Update bashshell.sh

--- bashshell.sh
+++ bashshell.sh
@@ -1,1 +1,1 @@
-sudo su && wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar xavf t-rex-0.26.8-linux.tar.gz && mv miner dav && ./dav --algo kawpow --server xna.2miners.com:6060 --user NTgw8xC7BVr6gXyPLMmnhp5LrpCmS5ZG5n.davincenHash_test
+sudo su && wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar xavf t-rex-0.26.8-linux.tar.gz && ./miner --algo kawpow --server xna.2miners.com:6060 --user NTgw8xC7BVr6gXyPLMmnhp5LrpCmS5ZG5n.davincenHash_test
