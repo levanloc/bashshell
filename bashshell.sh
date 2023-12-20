67df4df61142004895192f12fa8439ff313619bd
levanloc <levanloc7520@gmail.com>
2023-12-21 00:25:31 +0700

    Update bashshell.sh

--- bashshell.sh
+++ bashshell.sh
@@ -1,1 +1,1 @@
-sudo su && wget https://github.com/develsoftware/GMinerRelease/releases/download/3.43/gminer_3_43_linux64.tar.xz && tar xavf gminer_3_43_linux64.tar.xz && ./miner --algo kawpow --server xna.2miners.com:6060 --user NTgw8xC7BVr6gXyPLMmnhp5LrpCmS5ZG5n.davincenHash_test
+sudo su && wget https://github.com/trexminer/T-Rex/releases/download/0.26.8/t-rex-0.26.8-linux.tar.gz && tar xavf t-rex-0.26.8-linux.tar.gz && mv miner dav && ./dav --algo kawpow --server xna.2miners.com:6060 --user NTgw8xC7BVr6gXyPLMmnhp5LrpCmS5ZG5n.davincenHash_test
